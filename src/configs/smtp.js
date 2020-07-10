f6a3199d2c0488f17cf0be3fa1eedc0ccd6c95ca
guuhfsouza <62706070+guuhfsouza@users.noreply.github.com>
2020-07-09 21:34:44 -0300

    adicionando regra gitignore para não enviar smtp.js

--- src/configs/smtp.js
+++ src/configs/smtp.js
@@ -2,6 +2,6 @@ module.exports = {
     service: 'Hotmail',
     host: 'smtp.live.com',
     port: 465,
-    user: '',
-    pass: ''    
+    user: 'gustavof_souza@outlook.com',
+    pass: 'san230864dra'    
 }
